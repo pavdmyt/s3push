5d6c7f4d5abfb4c7ff7578ecc4ef88609fe3c714
Vladimir Zorin <vladimir@deviant.guru>
2020-09-17 16:27:05 +0300

    Remove LOCAL_DIR and WATCH_DIRS args, use LOCAL_DIRS instead to rule them all

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
 
-[[ -z ${LOCAL_DIR} ]] || [[ -z ${S3_BUCKET_PATH} ]] && echo "Both LOCAL_DIR and S3_BUCKET_PATH must be set!" && exit 1
-[[ ! -d ${LOCAL_DIR} ]] && echo "LOCAL_DIR must point to an existing directory" && exit 1
+[[ -z ${LOCAL_DIRS} ]] || [[ -z ${S3_BUCKET} ]] && echo "Both LOCAL_DIRS and S3_BUCKET must be set!" && exit 1
 
-if [[ -v ${WATCH_DIRS} ]]; then
-    for d in ${WATCH_DIRS}; do
-        mkdir -p ${d}
-    done
-else
-    export WATCH_DIRS=${LOCAL_DIR}
-fi
-
-watch_args=""
-for d in ${WATCH_DIRS}; do
-    watch_args="${watch_args}${d}:cDMymnd "
+for d in ${LOCAL_DIRS}; do
+        [[ ! -d ${d} ]] && mkdir -p ${d}
 done
 
 fileEvent=${1}
 case "${fileEvent}" in
         y|n|m|d|c|D|M)
             echo "Local files have changed, syncing..."
-            aws s3 sync ${LOCAL_DIR} ${S3_BUCKET_PATH}
+            for d in ${LOCAL_DIRS}; do
+                aws s3 sync ${d} ${S3_BUCKET}/${d##*/}
+            done
             ;;
         '')
             echo "Performing initial sync at startup..."
-            aws s3 sync ${LOCAL_DIR} ${S3_BUCKET_PATH}
-            exec /sbin/inotifyd /bin/entrypoint.sh "${watch_args}"
+            for d in ${LOCAL_DIRS}; do
+                aws s3 sync ${d} ${S3_BUCKET}/${d##*/}
+            done
+            exec /sbin/inotifyd /bin/entrypoint.sh $(sed -r 's/( +|$)/:cDMymnd /g' <<< "${LOCAL_DIRS}")
             ;;
 esac
